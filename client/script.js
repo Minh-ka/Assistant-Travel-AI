b62cefd0ebe991c173b8c6e8604e733dc1d8e34b
Minh Bui <oiconmadn@gmail.com>
2023-02-01 20:09:05 +0700

    commit

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server -> bot's response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('http://localhost:5000/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -99,6 +99,7 @@ const handleSubmit = async (e) => {
     const err = await response.text();
 
     messageDiv.innerHTML = "Something went wrong";
+    console.log(err);
     alert(err);
   }
 }
